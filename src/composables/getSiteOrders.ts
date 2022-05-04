8360ece7ef3d41ceecf972f4df5e4f5090bcb171
Blessings Chidambe <bchidambe@gmail.com>
2022-05-04 08:34:01 +0200

    Adding site pendinding orders

--- src/composables/getSiteOrders.ts
+++ src/composables/getSiteOrders.ts
@@ -1,4 +1,4 @@
-import { ref, watch } from "vue";
+import { ref } from "vue";
 import { useStore } from "@/store";
 import { Specimen } from "@/interfaces/Specimen";
 import { TestResult } from "@/interfaces/TestResult";
@@ -15,11 +15,6 @@ const SpecimensWithResults = ref<number[]>([]);
 const Patients = ref<PatientDash[]>([]);
 const Tests = ref<TestResult[]>([]);
 
-const BGSpecimens = ref<Specimen[]>([]);
-const BGTestWithResults = ref<TestResult[]>([]);
-const BGSpecimensWithResults = ref<number[]>([]);
-const BGPatients = ref<PatientDash[]>([]);
-const BGTests = ref<TestResult[]>([]);
 
 const getSiteOrders = () => {
 
@@ -37,9 +32,7 @@ const getSiteOrders = () => {
     axios.value
       .post("/ward/orders", {
         token: token.value,
-        ward:ward.value,
-        from:store.getters.fromDate,
-        to:store.getters.toDate
+        ward:ward.value
       })
       .then(function (response: any) {
         if (response.statusText === "OK") {
@@ -110,224 +103,8 @@ const getSiteOrders = () => {
       });
   };
 
-  const BGfetchOrders = () => {
 
-    axios.value
-      .post("/ward/orders", {
-        token: token.value,
-        ward:ward.value,
-        from:store.getters.fromDate,
-        to:store.getters.toDate
-      })
-      .then(function (response: any) {
-        if (response.statusText === "OK") {
-
-          if (response.data == "Invalid Token") logout()
-
-          code.value = response.data.code;
-
-          const responseData = response.data.data;
-
-
-          if (code.value == "200") {
-
-            BGTestWithResults.value = responseData.tests_with_results
-
-            BGTests.value = responseData.tests_with_details
-
-            BGTests.value.forEach(test => {
-
-              BGTestWithResults.value.forEach(result_test => {
-
-                if (test.id == result_test.id) {
-
-                    test = Object.assign(test, { class:"has"})
-                  
-                } 
-
-              });
-
-            });
-
-            BGTestWithResults.value.forEach(test => {
-
-              BGSpecimensWithResults.value.push(test.specimen_id)
-              
-            });
-
-
-            BGSpecimens.value = responseData.specimens
-
-            BGPatients.value = responseData.patients
-
-
-            for (let index = 0; index < BGSpecimens.value.length; index++) {
-
-              if (BGSpecimensWithResults.value.includes(BGSpecimens.value[index].id)) {
-
-                  BGSpecimens.value[index] = Object.assign(BGSpecimens.value[index], { class:"results-ready"})
-              
-              } else {
-
-                BGSpecimens.value[index] = Object.assign(BGSpecimens.value[index], { class:"from-db"})
-                
-              }
-              
-            }
-
-            message.value = response.data.message;
-
-          } else {
-
-            message.value = response.data.message;
-          }
-
-        }
-      })
-      .catch(function (error: any) {
-        message.value = error.message;
-      });
-  };
- 
-
-  setInterval(() => {
-
-      BGfetchOrders();
-
-
-  }, 10000);
-
-  watch(
-
-      () => [BGSpecimens.value],
-      () => {
-
-          Patients.value = BGPatients.value
-          Tests.value = BGTests.value
-          // Specimens.value = BGSpecimens.value
-          // BGfetchOrders();
-
-          if (store.getters.resultsFilter == "With Results") {
-            Specimens.value = filterWithResults()
-
-            if (store.getters.patientFilter.length > 0) Specimens.value = filterPatient()
-
-          } else if (store.getters.resultsFilter == "Without Results") {
-            Specimens.value = filterWithoutResults()
-
-            if (store.getters.patientFilter.length > 0) Specimens.value = filterPatient()
-
-          } else if (store.getters.resultsFilter == "All") {
-            Specimens.value = filterAllResults()
-
-            if (store.getters.patientFilter.length > 0) Specimens.value = filterPatient()
-            
-          }
-
-
-      }
-  );
-
-  watch(
-
-    () => [store.getters.fromDate],
-    () => {
-
-      Specimens.value.length = 0
-
-      BGfetchOrders();
-
-    }
-);
-
-watch(
-
-  () => [store.getters.toDate],
-  () => {
-
-    Specimens.value.length = 0
-
-    BGfetchOrders();
-
-  }
-);
-
-const filterAllResults = () => {
-  
-  return BGSpecimens.value
-}
-
-
-const filterWithResults = () => {
-  
-  return BGSpecimens.value.filter((specimen) => BGSpecimensWithResults.value.includes(specimen.id))
-}
-
-const filterWithoutResults = () => {
-
-  return BGSpecimens.value.filter((specimen) => !BGSpecimensWithResults.value.includes(specimen.id))
-}
-
-const filterPatient = () => {
-
-  let patients :PatientDash[] = []
-
-  patients = Patients.value.filter((pat) => pat.name.toLowerCase() == store.getters.patientFilter.toLowerCase())
-
-  let specimen_ids: number[] = []
-
-  patients.forEach(patient => {
-
-    if (!specimen_ids.includes(patient.specimen_id)) {
-
-      specimen_ids.push(patient.specimen_id)
-
-    }
-    
-  });
-
-  return BGSpecimens.value.filter((specimen) => specimen_ids.includes(specimen.id))
-}
-
-watch(
-  () => [store.getters.resultsFilter],
-  () => {
-    if (store.getters.resultsFilter == "With Results") {
-      Specimens.value = filterWithResults()
-
-      if (store.getters.patientFilter.length > 0) Specimens.value = filterPatient()
-
-    } else if (store.getters.resultsFilter == "Without Results") {
-      Specimens.value = filterWithoutResults()
-
-      if (store.getters.patientFilter.length > 0) Specimens.value = filterPatient()
-
-    } else if (store.getters.resultsFilter == "All") {
-      Specimens.value = filterAllResults()
-
-      if (store.getters.patientFilter.length > 0) Specimens.value = filterPatient()
-    }
-  }
-);
-
-watch(
-  () => [store.getters.patientFilter],
-  () => {
-
-    if (store.getters.patientFilter.length > 0){
-
-      Specimens.value = filterPatient()
-
-    } else {
-
-      BGfetchOrders();
-
-    }    
-    
-  }
-);
-
-  return { fetchOrders, BGfetchOrders, Specimens, Patients, Tests, BGSpecimensWithResults };
+  return { fetchOrders, Specimens, Patients, Tests };
 };
 
 export default getSiteOrders;
--- src/views/PendingOrdersPage.vue
+++ src/views/PendingOrdersPage.vue
@@ -8,13 +8,51 @@
       <collapse-tool-bar pageTitle="iBLIS | Pending Orders"/>
     
       <div id="container">
+        <ion-grid>
+          <ion-row>
+            <ion-col class="head-col"> Accession #</ion-col>
+            <ion-col class="head-col"> Test(s) </ion-col>
+            <ion-col class="head-col"> Specimen </ion-col>
+            <ion-col class="head-col"> Date Ordered </ion-col>
+          </ion-row>
 
-          <div id="flex-container">
+          <ion-row
+            class="cus-row"
+            v-for="Specimen in Specimens"
+            :key="Specimen.id"
+            :class="Specimen.class"
+            @click="ViewOrder(Specimen)"
+          >
+            <ion-col>
+              <ion-row
+                ><ion-col>{{ Specimen.accession_number }}</ion-col></ion-row
+              >
+            </ion-col>
+            <ion-col>
+              <ion-row v-for="Test in Tests" :key="Test.id">
+                <ion-col v-if="Test.specimen_id == Specimen.id">{{
+                  Test.test_name
+                }}</ion-col>
+              </ion-row>
+            </ion-col>
+            <ion-col>
+              <ion-row
+                ><ion-col>{{ Specimen.specimen_type }}</ion-col></ion-row
+              >
+            </ion-col>
+            <ion-col>
+              <ion-row
+                ><ion-col>{{ getDateCollected(Specimen) }}</ion-col>
+                </ion-row
+              >
+            </ion-col>
+           
+          </ion-row>
 
-            
-             
-          </div>
+          
+        </ion-grid>
       </div>
+
     </ion-content>
 
     <app-footer />
@@ -23,11 +61,13 @@
 </template>
 
 <script lang="ts">
-import { IonContent, IonPage } from '@ionic/vue';
+import { IonContent, IonPage, IonGrid, IonRow, IonCol } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import CollapseToolBar from "@/components/CollapseToolBar.vue"
 import ToolBar from "@/components/ToolBar.vue"
 import AppFooter from "@/components/AppFooter.vue"
+import GetSiteOrders from "@/composables/getSiteOrders";
+import { Specimen } from "@/interfaces/Specimen";
 
 export default defineComponent({
   name: 'PendingOrdersPage',
@@ -36,32 +76,44 @@ export default defineComponent({
     IonPage,
     ToolBar,
     CollapseToolBar,
-    AppFooter
+    AppFooter,
+    IonGrid, IonRow, IonCol,
   },
   setup(){
 
-  
+    const getDateCollected = (Specimen: Specimen) => {
 
-    return { }
+      let date_time: string = Specimen.date_of_collection;
+
+      return date_time.substring(0, 10);
+    };
+
+    const { fetchOrders, Specimens, Tests } = GetSiteOrders();
+
+    fetchOrders();
+    
+    return { Specimens, Tests, getDateCollected}
   }
 });
 </script>
 
 <style scoped>
 
-#flex-container {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content:center;
+ion-content {
+  --ion-background-color: #eee;
 }
 
-#container {
-  padding-top: 15px;
+.head-col {
+  text-align: left;
+  background: #ffffe2;
+  border-left: solid 1px rgb(202, 201, 201);
 }
 
-ion-content{
-  --ion-background-color:#eee;
+.is-new{
+  background-color: rgba(61, 231, 146, 0.281) !important;
 }
 
+.cus-row {
+  border-bottom: solid 1px rgb(202, 201, 201);
+}
 </style>
