65a1a2015f6b05261086bf121647824c3352c84a
Blessings Chidambe <bchidambe@gmail.com>
2022-05-26 20:59:11 +0200

    Adding print test Page

--- android/app/src/main/java/io/ionic/starter/IMovePrinterPlugin.java
+++ android/app/src/main/java/io/ionic/starter/IMovePrinterPlugin.java
@@ -13,6 +13,18 @@ public class IMovePrinterPlugin extends Plugin {
     @PluginMethod()
     public void PrintTest(PluginCall call) {
 
+        try {
+            
+            PrinterHelper.Barcode(PrinterHelper.BARCODE, PrinterHelper.code128, "2", "1", "50", "0", "340", true, "7", "0", "5", "0234565687");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PluginMethod()
+    public void Echo(PluginCall call) {
+
         String value = call.getString("value");
 
         JSObject ret = new JSObject();
